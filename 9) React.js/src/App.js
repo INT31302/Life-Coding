83ade923a06346ede92125a4f4051df6afe31df5
INT31302 <tkdwo287@gmail.com>
2020-08-10 23:30:02 +0900

    create component event

--- 9) React.js/src/App.js
+++ 9) React.js/src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: "welcome",
+      selected_content_id: 2,
       subject: { title: "WEB", sub: "world wide web!" },
+      welcome: { title: "welcome", desc: "Hello, React!!" },
       contents: [
         {
           id: 1,
@@ -29,17 +32,35 @@ class App extends Component {
     };
   }
   render() {
+    let _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      const index = this.state.selected_content_id - 1;
+      _title = this.state.contents[index].title;
+      _desc = this.state.contents[index].desc;
+    }
     return (
       <div className="App">
         <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
+          onChangePage={() => {
+            this.setState({ mode: "welcome" });
+          }}
         ></Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
-        ></Content>
+        <TOC
+          data={this.state.contents}
+          onChangePage={(id) => {
+            this.setState({
+              mode: "read",
+              selected_content_id: Number(id),
+            });
+          }}
+        ></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
